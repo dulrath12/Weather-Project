f486370c0bd82b44adefb2988040321d7d415577
Dakotah Ulrath <dulrath@live.com>
2022-05-15 21:15:37 -0400

    start of js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let response = fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=98ad45c0df8e458c68723b43de69d41d')
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=98ad45c0df8e458c68723b43de69d41d')
+
+
